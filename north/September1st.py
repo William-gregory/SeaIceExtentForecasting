f760e5ba4c39a6be6a149871a62e124fc054eef2
Will Gregory <William.gregory@outlook.com>
2022-02-03 14:31:42 -0500

    Update September1st.py

--- north/September1st.py
+++ north/September1st.py
@@ -142,7 +142,7 @@ def detrend(dataset):
             dataset['trend'] = trend
 
 def networks(dataset,key,latlon=True):
-    from CNs_backup.backups import CN_oldest as CN
+    import ComplexNetworks as CN
     dimXR = dataset['dt'].shape[0] ; dimYR = dataset['dt'].shape[1]
     network = CN.Network(dimX=dimXR,dimY=dimYR)
     CN.Network.tau(network, dataset['dt'], 0.01, 'Jul', key, home+'/DATA/')
